2a6284a5369186ef559879ab1a56d69bbaea00c4
yuchaemin2 <kusuri94@gmail.com>
2022-09-29 14:41:39 +0900

    CBV로 포스트 목록/상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ #IP 주소/blog
-    path('', views.index),  #IP 주소/blog
-    path('<int:pk>/', views.single_post_page)
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view())
+
+    #path('', views.index),  #IP 주소/blog
+    #path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def index(request):
-    posts1 = Post.objects.all().order_by('-pk')
-    return render(request, 'blog/index.html', {'posts': posts1})
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    #템플릿 모델명_list.html : post_list.html
+    #파라미터 모델명_list : post_list
 
-def single_post_page(request, pk):
-    post2 = Post.objects.get(pk=pk)
-    return render(request, 'blog/single_post_page.html', {'post':post2})
+class PostList(DetailView):
+    model = Post
+    # 템플릿 모델명_detail.html : post_detail.html
+    # 파라미터 모델명 : post
+
+#def index(request):
+#    posts1 = Post.objects.all().order_by('-pk')
+#    return render(request, 'blog/index.html', {'posts': posts1})
+
+#def single_post_page(request, pk):
+#    post2 = Post.objects.get(pk=pk)
+#    return render(request, 'blog/single_post_page.html', {'post':post2})
