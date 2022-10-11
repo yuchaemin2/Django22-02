b1b19831875d920b341d83f82254d058f80b4b2f
yuchaemin2 <kusuri94@gmail.com>
2022-10-11 17:37:26 +0900

    포스트 목록/상세 페이지에 첨부파일, 미리보기, 필터 적용하기

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import os
+
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
@@ -19,4 +22,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name) #basename: 파일 이름만 출력하는 함수
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1] #주어진 배열에서 가장 마지막 원소 -1
+        # a.txt -> a txt
+        # b.docx -> b docx
+        # c.xlsx -> c xlsx
+        # a.b.c.txt -> a b c txt
 
