7b899099deb21c2e89c2200c239a5e9aada6cfc0
yuchaemin2 <kusuri94@gmail.com>
2022-11-10 14:41:41 +0900

    태그 선택란 추가하기

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 # Create your views here.
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category'] # ,'tags'
 
     template_name = 'blog/post_update_form.html'
 
@@ -17,15 +18,37 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         else:
             raise PermissionDenied
 
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()  # 문자 앞 뒤에 있는 빈칸 없애기
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)  # 이미 존재하는 태그는 생성하지 않겠다
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+        return response
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = ';'.join(tags_str_list)
         context['categories'] = Category.objects.all() #templates를 위한 변수
         context['no_category_post_count'] = Post.objects.filter(category=None).count
         return context
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category'] # ,'tags'
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
@@ -34,7 +57,20 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):
             form.instance.author = current_user
-            return super(PostCreate,self).form_valid(form)
+            response = super(PostCreate,self).form_valid(form)
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str :
+                tags_str = tags_str.strip() #문자 앞 뒤에 있는 빈칸 없애기
+                tags_str = tags_str.replace(',',';')
+                tags_list = tags_str.split(';')
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t) #이미 존재하는 태그는 생성하지 않겠다
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode = True)
+                        tag.save()
+                    self.object.tags.add(tag)
+            return response
         else:
             return redirect('/blog/')
 
