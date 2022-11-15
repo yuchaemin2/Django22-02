47980977e211d19979b624daabdd352fde12de07
yuchaemin2 <kusuri94@gmail.com>
2022-11-15 16:53:25 +0900

    폼 모양 꾸미기

--- myDjangoPrj/settings.py
+++ myDjangoPrj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'blog',
     'single_pages'
 ]
@@ -122,6 +123,8 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
