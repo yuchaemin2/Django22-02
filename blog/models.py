0760751cad40738d15765df0ab7166386db6cce6
yuchaemin2 <kusuri94@gmail.com>
2022-11-29 16:40:34 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -76,3 +76,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://dummyimage.com/50x50/ced4da/6c757d.jpg'
